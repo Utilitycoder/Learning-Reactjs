566fdb4b4811c747ea3713b5ba68b20f95c4c5d8
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-02-28 15:20:05 +0100

    Update files

---
+++ my-travel-exp/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from '../images/airbnb1.png'
+
+export default function Nav() {
+    return (
+        <nav>
+            <img className="nav-logo" src={Logo} alt=""/>
+        </nav>
+    )
+}
