6eff57967a6598fb5941229d0f65bc52c4994df4
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-03 20:21:54 +0100

    Working on header component

--- portfolio-website/src/components/header/header.jsx
+++ portfolio-website/src/components/header/header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hi, I am</h5>
+        <h1>Lawal Abubakar Babatunde</h1>
+        <h5 className="text-light">Fullstack Blockchain Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll_down">Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
