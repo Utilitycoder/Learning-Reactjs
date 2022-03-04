cf379181972b481e5701de7832278588e26533e6
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-04 09:55:57 +0100

    Update

--- portfolio-website/src/components/about/about.jsx
+++ portfolio-website/src/components/about/about.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import Me from '../../assets/me.png'
+import {FiAward} from 'react-icons/fi'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const about = () => {
   return (
@@ -14,9 +17,24 @@ const about = () => {
       </div>
       <div className="about_content">
         <div className="about_cards">
-          <article>
-
+          <article className="about_card">
+            <FiAward className="about_card_icon"/>
+            <h5>Experience</h5>
+            <small>2 Years Working Experience</small>
           </article>
+          <article className="about_card">
+            <FiUsers className="about_card_icon"/>
+            <h5>Clients</h5>
+            <small>20+ Worldwide</small>
+          </article>
+          <article className="about_card">
+            <VscFolderLibrary className="about_card_icon"/>
+            <h5>Projects</h5>
+            <small>10+ Completed</small>
+          </article>
+          <p>
+            
+          </p>
         </div>
       </div>
     </section>
