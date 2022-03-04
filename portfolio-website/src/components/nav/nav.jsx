5e1395842e7f85ff1c43051a0eb7aa2e2d8ed14e
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-04 07:13:54 +0100

    Styling the Nav component

--- portfolio-website/src/components/nav/nav.jsx
+++ portfolio-website/src/components/nav/nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUsergroupAdd} from 'react-icons/ai'
+import {BiBrain} from 'react-icons/bi'
+import {MdOutlineElectricalServices} from 'react-icons/md'
+import {BsChatLeftDots} from 'react-icons/bs'
+
 
 const nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUsergroupAdd /></a>
+      <a href="#experience"><BiBrain /></a>
+      <a href="#services"><MdOutlineElectricalServices /></a>
+      <a href="#contact"><BsChatLeftDots /></a>
+    </nav>
   )
 }
 
