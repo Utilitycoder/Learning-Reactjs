fef1978e9498685a374d585e4857ce39c8a6b354
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-04 10:48:31 +0100

    Styled About

--- portfolio-website/src/components/about/about.jsx
+++ portfolio-website/src/components/about/about.jsx
@@ -10,31 +10,34 @@ const about = () => {
     <section id='about'>
       <h5>Get to Know</h5>
       <h2>About Me</h2>
-      <div className="about_me">
-        <div className="about_me-image">
-          <img src={Me} alt="About Me" />
+      <div className="container about_container">
+        <div className="about_me"> 
+          <div className="about_me-image">
+            <img src={Me} alt="About Me" />
+          </div>
         </div>
-      </div>
-      <div className="about_content">
-        <div className="about_cards">
-          <article className="about_card">
-            <FiAward className="about_card_icon"/>
-            <h5>Experience</h5>
-            <small>2 Years Working Experience</small>
-          </article>
-          <article className="about_card">
-            <FiUsers className="about_card_icon"/>
-            <h5>Clients</h5>
-            <small>20+ Worldwide</small>
-          </article>
-          <article className="about_card">
-            <VscFolderLibrary className="about_card_icon"/>
-            <h5>Projects</h5>
-            <small>10+ Completed</small>
-          </article>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FiAward className="about_card_icon"/>
+              <h5>Experience</h5>
+              <small>2 Years Working Experience</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_card_icon"/>
+              <h5>Clients</h5>
+              <small>20+ Worldwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_card_icon"/>
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
           <p>
-            
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
