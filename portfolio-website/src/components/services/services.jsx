9e733f99a47a08846d753c6aee9e0543a7a397c0
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-04 18:34:23 +0100

    Set Up services Component

--- portfolio-website/src/components/services/services.jsx
+++ portfolio-website/src/components/services/services.jsx
@@ -1,9 +1,112 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Can Do</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className="service">
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Web Development */}
+        <article className="service">
+          <div className="service_head">
+            <h3>Smart Contract Programming</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Smart Contract Programming */}
+        <article className="service">
+          <div className="service_head">
+            <h3>Web3 Marketing</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lorem, ipsum color sit amet</p>
+            </li>
+          </ul>
+        </article>
+        {/* End Web3 Marketing */}
+      </div>
+    </section>
   )
 }
 
