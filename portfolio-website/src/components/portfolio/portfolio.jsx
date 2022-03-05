baf059760d64f10d1f22865942e67fc57002bdee
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-05 15:39:48 +0100

    Styling portfolio component

--- portfolio-website/src/components/portfolio/portfolio.jsx
+++ portfolio-website/src/components/portfolio/portfolio.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
 
 const portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Zombiverze NFT" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
+          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
+
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Zombiverze NFT" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
+          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
+
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Zombiverze NFT" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
+          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
+
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Zombiverze NFT" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
+          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
+
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Zombiverze NFT" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
+          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
+
+        </article>
+      </div>
+    </section>
   )
 }
 
