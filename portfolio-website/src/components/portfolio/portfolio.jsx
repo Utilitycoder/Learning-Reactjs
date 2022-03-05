1e2349b8676638883c9afa08810f5a9620c75254
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-05 16:15:05 +0100

    Updated Data file

--- portfolio-website/src/components/portfolio/portfolio.jsx
+++ portfolio-website/src/components/portfolio/portfolio.jsx
@@ -2,6 +2,51 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
 
+const projects = [
+  {
+    id:1,
+    image: IMG1,
+    title: 'Zombieverze Website',
+    github: 'https://github.com',
+    link: 'https://zombieverze.club'
+  },
+  {
+    id:2,
+    image: IMG1,
+    title: 'Zombieverze Website',
+    github: 'https://github.com',
+    link: 'https://zombieverze.club'
+  },
+  {
+    id:3,
+    image: IMG1,
+    title: 'Zombieverze Website',
+    github: 'https://github.com',
+    link: 'https://zombieverze.club'
+  },
+  {
+    id:4,
+    image: IMG1,
+    title: 'Zombieverze Website',
+    github: 'https://github.com',
+    link: 'https://zombieverze.club'
+  },
+  {
+    id:5,
+    image: IMG1,
+    title: 'Zombieverze Website',
+    github: 'https://github.com',
+    link: 'https://zombieverze.club'
+  },
+  {
+    id:6,
+    image: IMG1,
+    title: 'Zombieverze Website',
+    github: 'https://github.com',
+    link: 'https://zombieverze.club'
+  },
+]
+
 const portfolio = () => {
   return (
     <section id='portfolio'>
@@ -9,56 +54,22 @@ const portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio_container">
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG1} alt="Zombiverze NFT" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item-cta">
-          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
-          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG1} alt="Zombiverze NFT" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item-cta">
-          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
-          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG1} alt="Zombiverze NFT" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item-cta">
-          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
-          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG1} alt="Zombiverze NFT" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item-cta">
-          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
-          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG1} alt="Zombiverze NFT" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item-cta">
-          <a href="https://github.com" target="_blank" rel="noreferrer" className="btn">Github</a>
-          <a href="https://zombieverze.club" target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
-          </div>
-        </article>
+       { 
+          projects.map(({id, image, title, github, link}) => {
+            return (
+              <article key={id} className="portfolio_item">
+              <div className="portfolio_item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>This is a portfolio item title</h3>
+              <div className="portfolio_item-cta">
+              <a href={github} target="_blank" rel="noreferrer" className="btn">Github</a>
+              <a href={link} target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
+              </div>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
