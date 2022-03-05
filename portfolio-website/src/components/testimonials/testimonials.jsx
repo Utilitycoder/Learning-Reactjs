afbefe6734bc0e90affd13445744cee189f2c5d0
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-05 16:47:44 +0100

    Imported object array

--- portfolio-website/src/components/testimonials/testimonials.jsx
+++ portfolio-website/src/components/testimonials/testimonials.jsx
@@ -1,5 +1,39 @@
 import React from 'react'
 import './testimonials.css'
+import Av1 from '../../assets/avatar1.png'
+import Av2 from '../../assets/avatar2.png'
+import Av3 from '../../assets/avatar3.png'
+import Av4 from '../../assets/avatar4.png'
+import Av5 from '../../assets/avatar5.png'
+
+const data = [
+  {
+    avatar: Av1,
+    name: "Kolapo Ishola",
+    Review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum eaque dolores labore culpa tempora pariatur, nihil quod corrupti inventore ex assumenda velit, provident eos enim dolorum voluptatibus blanditiis alias voluptatem!"
+  },
+  {
+    avatar: Av2,
+    name: "Kolapo Ishola",
+    Review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum eaque dolores labore culpa tempora pariatur, nihil quod corrupti inventore ex assumenda velit, provident eos enim dolorum voluptatibus blanditiis alias voluptatem!"
+  },
+  {
+    avatar: Av3,
+    name: "Kolapo Ishola",
+    Review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum eaque dolores labore culpa tempora pariatur, nihil quod corrupti inventore ex assumenda velit, provident eos enim dolorum voluptatibus blanditiis alias voluptatem!"
+  },
+  {
+    avatar: Av4,
+    name: "Kolapo Ishola",
+    Review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum eaque dolores labore culpa tempora pariatur, nihil quod corrupti inventore ex assumenda velit, provident eos enim dolorum voluptatibus blanditiis alias voluptatem!"
+  },
+  {
+    avatar: Av5,
+    name: "Kolapo Ishola",
+    Review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum eaque dolores labore culpa tempora pariatur, nihil quod corrupti inventore ex assumenda velit, provident eos enim dolorum voluptatibus blanditiis alias voluptatem!"
+  }
+]
+
 
 const testimonials = () => {
   return (
