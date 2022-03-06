6b57983f1814b78a2959e306870710b88ab89494
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-06 12:03:07 +0100

    Update contact style

--- portfolio-website/src/components/contact/contact.jsx
+++ portfolio-website/src/components/contact/contact.jsx
@@ -12,19 +12,19 @@ const contact = () => {
       <div className="container contact_container">
         <div className="contact_options">
         <article className="contact_option">
-          <MdOutlineEmail />
+          <MdOutlineEmail className="contact_option-icon"/>
           <h4>Email</h4>
           <h5>utility4all@gmail.com</h5>
           <a href="mailto:utility4all@gmail.com" target="_blank" rel="nonreferrer">Send a Message</a>
         </article>
         <article className="contact_option">
-          <RiMessengerLine />
+          <RiMessengerLine className="contact_option-icon"/>
           <h4>Messenger</h4>
           <h5>Lawal Abubakar Babatunde</h5>
           <a href="https://m.me/lawal.abubakr" target="_blank" rel="nonreferrer">Send a Message</a>
         </article>
         <article className="contact_option">
-          <BsWhatsapp />
+          <BsWhatsapp className="contact_option-icon"/>
           <h4>Whatsapp</h4>
           <h5>+2348098634567</h5>
           <a href="https://api.whatsapp.com/send?phone=+2348062702016" target="_blank" rel="nonreferrer">Send a Message</a>
