574a1bf7a7fbbcf213c95ebf8369cb1552977b0b
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-08 15:35:02 +0100

    Refactored CardJs code

--- my-travel-exp/src/components/Card.js
+++ my-travel-exp/src/components/Card.js
@@ -5,7 +5,7 @@ import Star from '../images/star.png'
 
 export default function Card(props) {
     let badgeText
-    if (props.item.Openspots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
     } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
@@ -17,8 +17,8 @@ export default function Card(props) {
             <img src={props.item.coverImg} alt="" className="card--image" />
             <div className="card--stats">
                 <img src={Star} alt="" className="card--star" />
-                <span> {props.item.rating} </span>
-                <span className="gray">({props.item.stat.reviewCount}) • </span>
+                <span> {props.item.stats.rating} </span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
                 <span className="gray">{props.item.location}</span>
             </div>
             <p className="card-title">{props.item.title}</p>
