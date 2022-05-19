9c6c77badca2f8ff07690c349b2eacea6514e811
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-05-19 09:20:32 +0100

    updated app

---
+++ form-context/src/Header.js
@@ -0,0 +1,15 @@
+import {UserContextConsumer} from "./userContext"
+
+export default function Header() {
+    return (
+        <header>
+            <UserContextConsumer>
+                {
+                    ({username}) => (
+                        <p>Welcome, {username}!</p>
+                    )
+                }
+            </UserContextConsumer>
+        </header>
+    )
+}
